c1b39e2adaf726d444cfb3d73010a7bba6e1d41c
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2019-08-19 17:37:00 +0900

    chore: add eslint-prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
   env: {
     es6: true,
     node: true,
