29ccaa100c4c603f8391cabecbfb4f38153d3684
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2019-12-24 18:26:33 +0900

    fix: lint issue

--- examples/hn/services/HackerNewsItem.ts
+++ examples/hn/services/HackerNewsItem.ts
@@ -3,15 +3,15 @@ import fetch from "isomorphic-unfetch";
 import Cache from "node-cache";
 
 export type HNItemBody = {
-  id: number, 
-  deleted: boolean,
-  type: "job" | "story" | "comment" | "poll" | "pollopt",
-  by: string,
-  time: number,
-  text: string,
-  dead: boolean,
-  url: string,
-  title: string,
+  id: number;
+  deleted: boolean;
+  type: "job" | "story" | "comment" | "poll" | "pollopt";
+  by: string;
+  time: number;
+  text: string;
+  dead: boolean;
+  url: string;
+  title: string;
 };
 export type HackerNewsItemResponse = Response<{}, HNItemBody>;
 
@@ -28,22 +28,19 @@ export default class HackerNewsItem extends Service {
   ): Promise<HackerNewsItemResponse> {
     const cacheData = this.cache.get(`${cachekeyPrefix}-${request.query.id}`);
     if (cacheData) {
-      return new Response(
-        {},
-        cacheData as HNItemBody,
-      );
+      return new Response({}, cacheData as HNItemBody);
     }
-    const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${request.query.id}.json`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8"
+    const res = await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${request.query.id}.json`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
       }
-    });
+    );
     const data: HNItemBody = await res.json();
     this.cache.set(`${cachekeyPrefix}-${request.query.id}`, data);
-    return new Response(
-      {},
-      data,
-    );
+    return new Response({}, data);
   }
 }
--- examples/hn/services/HackerNewsList.ts
+++ examples/hn/services/HackerNewsList.ts
@@ -8,25 +8,26 @@ export default class HackerNewsList extends Service {
   constructor(config: object) {
     super("hnlist", config);
   }
-  async read(
-    _: Request<{}, {}, null>
-  ): Promise<HackerNewsListResponse> {
-    const res = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8"
+  async read(_: Request<{}, {}, null>): Promise<HackerNewsListResponse> {
+    const res = await fetch(
+      "https://hacker-news.firebaseio.com/v0/topstories.json",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
       }
-    });
-    const data: Array<number> = await res.json();
-    const nextReqs = data.slice(0, 10).map((id) => new ClientRequest<{},{ id: number },null>("hnitem", { 
-      headers: {}, 
-      query: { id: id }, 
-      body: null,
-    }));
-    const resp = new Response(
-      {},
-      data,
     );
+    const data: Array<number> = await res.json();
+    const nextReqs = data.slice(0, 10).map(
+      id =>
+        new ClientRequest<{}, { id: number }, null>("hnitem", {
+          headers: {},
+          query: { id: id },
+          body: null
+        })
+    );
+    const resp = new Response({}, data);
     resp.setNextReqs(...nextReqs);
     return resp;
   }
