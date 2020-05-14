c630f67026bc3f8feae7a82070de01497835e5a4
MaxMEllon <maxmellon1994@gmail.com>
2020-05-15 08:03:14 +0900

    refactor: eslint

--- packages/specter/esm/specter.d.ts
+++ packages/specter/esm/specter.d.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from "express";
 import Service from "./service";
 export default class Specter {
-  private static services;
-  static registerService(service: Service): void;
-  static getService(name: string): Service;
-  static isRegistered(name: string): boolean;
-  static createMiddleware(options: any): RequestHandler;
+    private static services;
+    static registerService(service: Service): void;
+    static getService(name: string): Service;
+    static isRegistered(name: string): boolean;
+    static createMiddleware(options: any): RequestHandler;
 }
